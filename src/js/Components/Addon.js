9081111797b086cbca1a0591cf102a538eeb4362
Stevensc92 <stevensc92@gmail.com>
2023-06-17 14:15:23 +0200

    ajout d'1 colonne de plus pour le menu

--- src/js/Components/Addon.js
+++ src/js/Components/Addon.js
@@ -482,7 +482,7 @@ const Addon = window.Addon = {
     },
 
     splitFastChangeMenu: function () {
-        Addon.injectStyle('nav li.dropdown .dropdown-menu .dropdown-menu { columns: 3; }');
+        Addon.injectStyle('nav li.dropdown .dropdown-menu .dropdown-menu { columns: 4; }');
     },
 
     addBtnInstantHeal70: function () {
