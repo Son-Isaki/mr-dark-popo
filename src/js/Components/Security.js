12eb898b3018bacfbb43de971a5b6b26f2ce7c3c
Stevensc92 <stevensc92@gmail.com>
2023-06-18 20:43:03 +0200

    update security for vh

--- src/js/Components/Security.js
+++ src/js/Components/Security.js
@@ -1,38 +1,5 @@
 const Security = window.Security = {
     hasAccess: false,
-    whiteList: [
-        14, // Excasis
-        6, // Laman
-        5, // Ice.Thales
-        12, // Konoshi
-        84, // VaLyon
-        21, // Regulus
-        41, // BloodChopper
-        24, // Apollo
-        22, // Testeur Officiel
-        4, // Daiki
-        25, // PolonaisDeter
-        46, // Vaqui
-        33, // SuperBro
-        15, // ninik
-        13, // BigSmileIci
-        91, // KyanBrand
-        82, // Carbonic
-        49, // Nonomimi
-        10, // TensaZangetsu
-        60, // wolf
-        216, // kiranoma
-        590, // Sg4_tv
-        57, // Rominov
-        435, // Shoriu
-        68, // Natsuro
-        52, // BdX_D
-        89, // Yoohyeon
-        655, // Ragusen
-        669, // Kono
-        637, // Luxuko
-        644, // NOUNOURS
-    ],
 
     init: function () {
         const $this = this;
@@ -69,11 +36,13 @@ const Security = window.Security = {
             return false;
         }
 
-        let idUser = parseInt(this.getIdUser());
+        let clanLink = $('a[href="/regarderClan/4"]');
 
-        if (this.whiteList.indexOf(idUser) === '-1') {
+        if (clanLink.length === 0)
             return false;
-        }
+
+        if (clanLink.html() !== "Vulcain's Hammer")
+            return false;
 
         this.hasAccess = true;
     },
