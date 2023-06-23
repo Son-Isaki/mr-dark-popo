34b8e9df38adeb89ba61570e749c182ef5736099
Stevensc92 <stevensc92@gmail.com>
2023-06-24 00:50:39 +0200

    ajout avatar + wip update option fight auto

--- src/js/Components/Fights.js
+++ src/js/Components/Fights.js
@@ -225,15 +225,13 @@ const Fights = window.FightsComponent = {
             $('.safeZone').trigger('click');
         }
 
-        if (LocalStorage.get(Options.OPTIONS.autoFight, 'false') !== 'false') {
+        if (LocalStorage.get(Automate.OPTIONS.autoFightRunning, 'false') !== 'false') {
             if (LocalStorage.get('auto-fight-safe-zone', 'false') !== 'false') {
                 Automate.moveToSafeZone(Events.AutoFightSwitchChar);
             } else {
                 Events.trigger(Events.AutoFightSwitchChar);
             }
-
         }
-
     },
 
     ajaxFight: function () {
